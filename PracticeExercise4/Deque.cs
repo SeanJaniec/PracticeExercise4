b9d069cfff133fab51d22e160282f4089b845e12
SeanJaniec <78286187+SeanJaniec@users.noreply.github.com>
2021-10-25 00:09:09 -0500

    Update Deque.cs

--- PracticeExercise4/Deque.cs
+++ PracticeExercise4/Deque.cs
@@ -1,10 +1,49 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 namespace PracticeExercise4
 {
-    public class Deque<T> 
+    public class Deque<T> : IDeque<T>
     {
+        private Deque<T> deque;
         public Deque()
         {
+            deque = new Deque<T>();
+        }
+
+        public bool IsEmpty => deque.IsEmpty ? true : false;
+
+        public int Length => deque.Length;
+
+        public T Front => deque.Length != 0 ? deque.Front : throw new EmptyQueueException();
+
+        public T Back => deque.Length != 0 ? deque.Back : throw new EmptyQueueException();
+
+
+
+        public void AddBack(T item)
+        {
+            deque.AddBack(item);
+        }
+
+        public void AddFront(T item)
+        {
+            deque.AddFront(item);
+        }
+
+        public T RemoveBack()
+        {
+            
+
+            return deque.RemoveBack();
+
+
+        }
+
+        public T RemoveFront()
+        {
+            return deque.RemoveFront();
+
         }
     }
 }
+
